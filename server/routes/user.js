3b4bf2246863f5fa78e37f954e6560c72d0e1d0e
alvarotorresc <alvarotorrescarrasco@gmail.com>
2020-02-03 20:21:52 +0100

    put y delete

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const _ = require('lodash');
 const User = require('../models/user');
 const app = express();
 
 
 
-app.get('/user', function (req, res) {
-    res.json('get user')
+app.get('/user', async (req, res) => {
+
+    let from = req.query.from || 0;
+    from = Number(from);
+
+    let limit = req.query.limit || 0;
+    limit = Number(limit);
+
+
+    User.find({}, 'name mail role state google img')
+        .skip(from)
+        .limit(limit)
+        .exec(async (err, users) => {
+            try {
+                User.count({}, (err, count) => {
+                    res.json({
+                        ok: true,
+                        users,
+                        count
+                    });
+                })
+            } catch (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+        })
 });
 
 app.post('/user', async (req, res) => {
@@ -32,14 +59,51 @@ app.post('/user', async (req, res) => {
     }
 });
 
-app.put('/user/:id', function (req, res) {
+app.put('/user/:id', async (req, res) => {
     let id = req.params.id;
-    res.json({
-        id
-    })
+    let body = _.pick(req.body, ['name', 'mail', 'password', 'img', 'role', 'state']);
+
+    User.findByIdAndUpdate(id, body, { new: true, runValidators: true }, async (err, userDB) => {
+        try {
+            res.json({
+                ok: true,
+                editUser: await userDB
+            })
+        } catch (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+    });
 });
-app.delete('/user', function (req, res) {
-    res.json('delete user')
+app.delete('/user/:id', async (req, res) => {
+    let id = req.params.id;
+
+    User.findByIdAndRemove(id, async (err, deletedUser) => {
+        if (deletedUser) {
+            return res.status(400).json({
+                ok: false,
+                error: {
+                    message: 'User not found'
+                }
+            })
+        }
+
+        try {
+
+            res.json({
+                ok: true,
+                deleteUser: await deletedUser
+            })
+        } catch (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+    });
+
 });
 
 module.exports = app;

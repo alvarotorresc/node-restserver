78fce178950912a57f49ec4937a2939c053c2acb
alvarotorresc <alvarotorrescarrasco@gmail.com>
2020-02-09 19:54:18 +0100

    crud categories

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,18 +1,53 @@
 const express = require('express');
 
-let { verifyToken } = require('../middelwares/autentification');
+let { verifyToken, verifyAdminRole } = require('../middelwares/autentification');
 
 let app = express();
 
 let Category = require('../models/category');
 
 
-app.get('/category', (req, res) => {
+app.get('/category', verifyToken, (req, res) => {
     //all categories
+    Category.find({})
+        .exec((err, categories) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                categories
+            })
+        })
+
 });
 
-app.get('/category/:id', (req, res) => {
+app.get('/category/:id', verifyToken, (req, res) => {
     //one category by id
+    const id = req.params.id;
+
+    Category.findById(id, (err, categoryDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!categoryDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            category: categoryDB
+        })
+    })
 });
 
 
@@ -76,9 +111,28 @@ app.put('/category/:id', (req, res) => {
 
 });
 
-app.put('/category/:id', (req, res) => {
-    //only an admin
-    //category.find
+app.delete('/category/:id', [verifyToken, verifyAdminRole], (req, res) => {
+    const id = req.params.id;
+    Category.findByIdAndRemove(id, (err, categoryDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!categoryDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'id not found'
+                }
+            });
+        }
+        res.json({
+            ok: true,
+            message: 'Categorie deleted'
+        })
+    })
 });
 
 
